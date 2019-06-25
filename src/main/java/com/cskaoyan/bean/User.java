dcade80f344f01e74848509d9a433090dd4d0f4e
xiegongju <862047251@qq.com>
2019-06-25 22:38:51 +0800

    user

---
+++ src/main/java/com/cskaoyan/bean/User.java
@@ -0,0 +1,14 @@
+package com.cskaoyan.bean;
+
+public class User {
+    int id;
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
